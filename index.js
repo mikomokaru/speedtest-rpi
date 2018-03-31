c68ed7c18ea07e21c533498d19d2e87757e7cb1b
田中陽里 <hisato@tanaka-mbp.local>
2018-03-31 17:50:13 +0900

    first

---
+++ index.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const fs = require('fs')
+const os = require('os')
+const _ = require('lodash')
+const Bacon = require('baconjs')
+const AWS = require('aws-sdk')
+AWS.config.update({
+	accessKeyId: 'AKIAJKDTRSO4GNSXK6BA',
+	secretAccessKey: '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',
+	region: 'ap-northeast-1'
+})
+
+const s3 = require('s3')
+const speedTest = require('speedtest-net')
+
+// console.log(
+// 	_.chain(os.networkInterfaces())
+// 	.pick(['wlan0','en0'])
+// 	//.head()
+// 	.value()
+// )
+
+const run = (param)=> new Promise((resolve,reject)=> {
+	const t = speedTest(param);
+	t.on('data',resolve)
+	t.on('error',reject)
+}
+)
+
+const machineId = 'machine-Id'
+
+const syncParams = {
+	localDir: './data',
+	deleteRemoved: false,
+	s3Params: {
+		Bucket: 'wifi-speed.yamaokaya.net',
+		Prefix: `results/machineId=${machineId}/`
+	}
+}
+
+const sync = ()=> new Promise((resolve,reject)=>{
+	const uploader = client.uploadDir(syncParams)
+	uploader.on('end',resolve)
+	uploader.on('error',reject)
+})
+
+const client = s3.createClient({s3Client: new AWS.S3({signatureVersion: 'v4'})})
+
+Bacon.fromPromise(run({maxTime: 5000}))
+.map((v)=>
+	_.assign(
+		v,
+		{
+			interfaces: 
+			_.chain(os.networkInterfaces())
+			.pick(['wlan0','en0'])
+			.value()
+		}
+	)
+)
+.map((v)=>
+	fs.writeFileSync(`data/${Date.now()}.json`,JSON.stringify(v))
+)
+.map(sync)
+.flatMap(Bacon.fromPromise)
+.onValue(console.log)
+
+
+// var test = speedTest({maxTime: 5000})
+
+// test.on('data', data => {
+//   console.dir(data);
+// });
+
+// test.on('error', err => {
+//   console.error(err);
+// });
